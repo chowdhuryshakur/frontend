58e7d8d3d7e0369387d9075dff5988c09274a327
chowdhuryshakur <chowdhuryshakur@gmail.com>
2020-04-14 08:01:52 +0600

    make it better

--- manage-meeting-frontend/src/app/components/sidebar/sidebar.component.ts
+++ manage-meeting-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -15,7 +15,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/notifications', title: 'Invited Meeting',  icon:'ui-1_email-85', class: '' },
     { path: '/user-profile', title: 'Objection',  icon:'ui-2_chat-round', class: '' }
 
-   /* { path: '/icons', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
+   /*
     { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
 */
